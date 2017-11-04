5bb1a9990eeb3a707e2ef60df7168b1919e78b91
Eduardo Sanchez-Ros <esanchezros@yahoo.es>
2017-11-04 21:44:42 +0000

    Added license info and copyright notice

--- quickfixj-spring-boot-starter/src/main/java/io/allune/quickfixj/spring/boot/starter/autoconfigure/server/QuickFixJServerAutoConfiguration.java
+++ quickfixj-spring-boot-starter/src/main/java/io/allune/quickfixj/spring/boot/starter/autoconfigure/server/QuickFixJServerAutoConfiguration.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.allune.quickfixj.spring.boot.starter.autoconfigure.server;
 
 import io.allune.quickfixj.spring.boot.starter.autoconfigure.QuickFixJBootProperties;
@@ -28,18 +44,16 @@ import javax.management.ObjectName;
 @Conditional(QuickFixJServerAutoConfiguration.ServerConfigAvailableCondition.class)
 public class QuickFixJServerAutoConfiguration {
 
-    static final String SYSTEM_VARIABLE_QUICKFIXJ_SERVER_CONFIG = "quickfixj.server.config";
+    private static final String SYSTEM_VARIABLE_QUICKFIXJ_SERVER_CONFIG = "quickfixj.server.config";
 
-    static final String FILESYSTEM_QUICKFIXJ_SERVER_CONFIG = "file:./quickfixj-server.cfg";
-
-    static final String CLASSPATH_QUICKFIXJ_SERVER_CONFIG = "classpath:/quickfixj-server.cfg";
+    private static final String QUICKFIXJ_SERVER_CONFIG = "quickfixj-server.cfg";
 
     @Bean
     @ConditionalOnMissingBean(name = "serverSessionSettings")
     public SessionSettings serverSessionSettings(QuickFixJBootProperties properties) {
         return SessionSettingsLocator.loadSettings(properties.getClient().getConfig(),
-                SYSTEM_VARIABLE_QUICKFIXJ_SERVER_CONFIG, FILESYSTEM_QUICKFIXJ_SERVER_CONFIG,
-                CLASSPATH_QUICKFIXJ_SERVER_CONFIG);
+                SYSTEM_VARIABLE_QUICKFIXJ_SERVER_CONFIG, "file:./" + QUICKFIXJ_SERVER_CONFIG,
+                "classpath:/" + QUICKFIXJ_SERVER_CONFIG);
     }
 
     @Bean

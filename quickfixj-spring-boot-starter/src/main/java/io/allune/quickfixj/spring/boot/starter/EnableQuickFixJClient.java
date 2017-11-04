4a9c53fd8b504877b0cd51f6e9164f041a23f406
Eduardo Sanchez-Ros <eduardo.sanchez@engineering.appsbroker.com>
2017-11-04 21:44:42 +0000

    Added license info and copyright notice

--- quickfixj-spring-boot-starter/src/main/java/io/allune/quickfixj/spring/boot/starter/EnableQuickFixJClient.java
+++ quickfixj-spring-boot-starter/src/main/java/io/allune/quickfixj/spring/boot/starter/EnableQuickFixJClient.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.allune.quickfixj.spring.boot.starter;
 
 import io.allune.quickfixj.spring.boot.starter.autoconfigure.client.QuickFixJClientMarkerConfiguration;
@@ -6,7 +22,7 @@ import org.springframework.context.annotation.Import;
 import java.lang.annotation.*;
 
 /**
- * Sets up a Fix Initiator (Client), configures the connection to communicate to a Fix Acceptor (Server), handles the
+ * Sets up an Initiator (Client), configures the connection to communicate to an Acceptor (Server), handles the
  * connection's lifecycle and processes messages for all sessions
  *
  * @author Eduardo Sanchez-Ros
--- quickfixj-spring-boot-starter/src/main/java/io/allune/quickfixj/spring/boot/starter/autoconfigure/client/QuickFixJClientAutoConfiguration.java
+++ quickfixj-spring-boot-starter/src/main/java/io/allune/quickfixj/spring/boot/starter/autoconfigure/client/QuickFixJClientAutoConfiguration.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.allune.quickfixj.spring.boot.starter.autoconfigure.client;
 
 import io.allune.quickfixj.spring.boot.starter.autoconfigure.QuickFixJBootProperties;
@@ -29,18 +45,16 @@ import javax.management.ObjectName;
 @Conditional(QuickFixJClientAutoConfiguration.ClientConfigAvailableCondition.class)
 public class QuickFixJClientAutoConfiguration {
 
-    static final String SYSTEM_VARIABLE_QUICKFIXJ_CLIENT_CONFIG = "quickfixj.client.config";
+    private static final String SYSTEM_VARIABLE_QUICKFIXJ_CLIENT_CONFIG = "quickfixj.client.config";
 
-    static final String FILESYSTEM_QUICKFIXJ_CLIENT_CONFIG = "file:./quickfixj-client.cfg";
-
-    static final String CLASSPATH_QUICKFIXJ_CLIENT_CONFIG = "classpath:/quickfixj-client.cfg";
+    private static final String QUICKFIXJ_CLIENT_CONFIG = "quickfixj-client.cfg";
 
     @Bean
     @ConditionalOnMissingBean(name = "clientSessionSettings")
     public SessionSettings clientSessionSettings(QuickFixJBootProperties properties) {
         return SessionSettingsLocator.loadSettings(properties.getClient().getConfig(),
-                SYSTEM_VARIABLE_QUICKFIXJ_CLIENT_CONFIG, FILESYSTEM_QUICKFIXJ_CLIENT_CONFIG,
-                CLASSPATH_QUICKFIXJ_CLIENT_CONFIG);
+                SYSTEM_VARIABLE_QUICKFIXJ_CLIENT_CONFIG, "file:./" + QUICKFIXJ_CLIENT_CONFIG,
+                "classpath:/" + QUICKFIXJ_CLIENT_CONFIG);
     }
 
     @Bean
@@ -109,7 +123,7 @@ public class QuickFixJClientAutoConfiguration {
 
         ClientConfigAvailableCondition() {
             super(SYSTEM_VARIABLE_QUICKFIXJ_CLIENT_CONFIG, "quickfixj.client", "config",
-                    FILESYSTEM_QUICKFIXJ_CLIENT_CONFIG, CLASSPATH_QUICKFIXJ_CLIENT_CONFIG);
+                    "file:./" + QUICKFIXJ_CLIENT_CONFIG, "classpath:/" + QUICKFIXJ_CLIENT_CONFIG);
         }
     }
 }

d253042d5983faf6998e57236cb0d8eee45ba902
Eduardo Sanchez-Ros <esanchezros@yahoo.es>
2017-11-05 00:26:00 +0000

    Added QuickFixJAutoConfigFailureAnalyzerTest and excluded some classes from cobertura report

---
+++ quickfixj-spring-boot-starter/src/test/java/io/allune/quickfixj/spring/boot/starter/failureanalyzer/QuickFixJAutoConfigFailureAnalyzerTest.java
@@ -0,0 +1,57 @@
+package io.allune.quickfixj.spring.boot.starter.failureanalyzer;
+
+import io.allune.quickfixj.spring.boot.starter.exception.ConfigurationException;
+import io.allune.quickfixj.spring.boot.starter.exception.QuickFixJBaseException;
+import io.allune.quickfixj.spring.boot.starter.exception.SettingsNotFoundException;
+import org.junit.Test;
+import org.springframework.boot.diagnostics.FailureAnalysis;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class QuickFixJAutoConfigFailureAnalyzerTest {
+
+    @Test
+    public void shouldAnalyzeConfigurationException() {
+
+        // Given
+        QuickFixJAutoConfigFailureAnalyzer analyzer = new QuickFixJAutoConfigFailureAnalyzer();
+        ConfigurationException exception = new ConfigurationException("Error", new RuntimeException());
+
+        // When
+        FailureAnalysis analyze = analyzer.analyze(null, exception);
+
+        assertThat(analyze.getAction()).contains("Please configure your QuickFixJ settings");
+        assertThat(analyze.getDescription()).contains("A configuration error has been detected in the QuickFixJ settings provided.");
+        assertThat(analyze.getCause()).isEqualTo(exception);
+    }
+
+    @Test
+    public void shouldAnalyzeSettingsNotFoundException() {
+
+        // Given
+        QuickFixJAutoConfigFailureAnalyzer analyzer = new QuickFixJAutoConfigFailureAnalyzer();
+        SettingsNotFoundException exception = new SettingsNotFoundException("Error", new RuntimeException());
+
+        // When
+        FailureAnalysis analyze = analyzer.analyze(null, exception);
+
+        assertThat(analyze.getAction()).contains("Please provide a QuickFixJ settings file");
+        assertThat(analyze.getDescription()).contains("The QuickFixJ settings file could be found.");
+        assertThat(analyze.getCause()).isEqualTo(exception);
+    }
+
+    @Test
+    public void shouldAnalyzeAnyException() {
+
+        // Given
+        QuickFixJAutoConfigFailureAnalyzer analyzer = new QuickFixJAutoConfigFailureAnalyzer();
+        QuickFixJBaseException exception = new QuickFixJBaseException("Error message", new RuntimeException());
+
+        // When
+        FailureAnalysis analyze = analyzer.analyze(null, exception);
+
+        assertThat(analyze.getAction()).contains("Error message");
+        assertThat(analyze.getDescription()).contains("Error message");
+        assertThat(analyze.getCause()).isEqualTo(exception);
+    }
+}

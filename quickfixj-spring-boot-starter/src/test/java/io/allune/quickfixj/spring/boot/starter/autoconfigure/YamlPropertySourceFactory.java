5ed614b1211a3b6322a1282c7e63d288a4dc10d7
Eduardo Sanchez-Ros <esanchezros@yahoo.es>
2022-02-10 13:24:10 +0100

    Allow quickfixj config to be specified in spring properties #78 (#81)

---
+++ quickfixj-spring-boot-starter/src/test/java/io/allune/quickfixj/spring/boot/starter/autoconfigure/YamlPropertySourceFactory.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2017-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.allune.quickfixj.spring.boot.starter.autoconfigure;
+
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.support.EncodedResource;
+import org.springframework.core.io.support.PropertySourceFactory;
+
+import java.util.Objects;
+import java.util.Properties;
+
+@SuppressWarnings("NullableProblems")
+public class YamlPropertySourceFactory implements PropertySourceFactory {
+
+	@Override
+	public PropertySource<?> createPropertySource(String name, EncodedResource encodedResource) {
+		YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
+		factory.setResources(encodedResource.getResource());
+
+		Properties properties = factory.getObject();
+		return new PropertiesPropertySource(
+				Objects.requireNonNull(encodedResource.getResource().getFilename()),
+				Objects.requireNonNull(properties)
+		);
+	}
+}
